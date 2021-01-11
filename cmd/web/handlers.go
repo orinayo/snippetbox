2d7c527b7768ceca253bcda7d29cbf4725a8e09f
Orinayo Oyelade <orinayooyelade@gmail.com>
2021-01-11 11:41:07 -0800

    use pat for route handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,11 +10,6 @@ import (
 )
 
 func (app *application) home(resWriter http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
-		app.notFound(resWriter)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(resWriter, err)
@@ -25,7 +20,9 @@ func (app *application) home(resWriter http.ResponseWriter, req *http.Request) {
 }
 
 func (app *application) showSnippet(resWriter http.ResponseWriter, req *http.Request) {
-	id, err := strconv.Atoi(req.URL.Query().Get("id"))
+	// Pat doesn't strip the colon from the named capture key, so we need to
+	// get the value of ":id" from the query string instead of "id".
+	id, err := strconv.Atoi(req.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(resWriter)
 		return
@@ -44,13 +41,11 @@ func (app *application) showSnippet(resWriter http.ResponseWriter, req *http.Req
 	app.render(resWriter, req, "show.page.tmpl", &templateData{Snippet: snippet})
 }
 
-func (app *application) createSnippet(resWriter http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		resWriter.Header().Set("Allow", http.MethodPost)
-		app.clientError(resWriter, http.StatusMethodNotAllowed)
-		return
-	}
+func (app *application) createSnippetForm(resWriter http.ResponseWriter, req *http.Request) {
+	resWriter.Write([]byte("Create a new snippet"))
+}
 
+func (app *application) createSnippet(resWriter http.ResponseWriter, req *http.Request) {
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := "7"
@@ -61,5 +56,5 @@ func (app *application) createSnippet(resWriter http.ResponseWriter, req *http.R
 		return
 	}
 
-	http.Redirect(resWriter, req, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(resWriter, req, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
